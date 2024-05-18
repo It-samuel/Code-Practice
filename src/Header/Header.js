f5d7148bc7fd6c062c3e76391e521e75ff270012
it-samuel <samuelhappiness360@gmail.com>
2024-05-18 20:34:15 +0100

    initial commit

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Header() {
+    return(
+        <div className="w-screen bg-blue-500 h-20">
+            <div>
+                <h1 className="text-3xl text-white font-bold p-5">Welcome to Travis</h1>
+            </div>
+
+        </div>
+    )
+}
+
+
